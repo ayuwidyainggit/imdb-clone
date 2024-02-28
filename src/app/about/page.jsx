c3fc2378a4bf4b9ee3532ff127a3c46bd9fe4808
ayuwidyainggit <widyaayu560@gmail.com>
2024-02-28 23:40:59 +0700

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,20 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl  font-medium text-amber-600 ">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+        cumque illo aliquid id debitis nostrum magnam, itaque accusamus culpa,
+        laborum quasi, deleniti vero optio impedit sequi fugiat ad saepe quos
+        illum natus necessitatibus earum! Quisquam inventore vel dolore rem
+        recusandae illo ipsam laudantium soluta incidunt earum hic voluptatibus
+        repudiandae, dolor culpa nesciunt tempore libero molestias rerum modi
+        deleniti qui non optio! Facere amet nobis sit. Velit itaque corrupti, a
+        dolorem explicabo cum esse earum deserunt! Dignissimos mollitia error ad
+        necessitatibus saepe deserunt pariatur in, autem laborum distinctio
+        magni? Explicabo, consequuntur nihil? Eius magnam vel, rerum placeat
+        possimus tenetur facere excepturi?
+      </p>
+    </div>
+  );
+}
