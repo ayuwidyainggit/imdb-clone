b9a34d807762e8826c854213aea0961b85e56b77
ayuwidyainggit <widyaayu560@gmail.com>
2024-02-29 00:11:26 +0700

    create the navbar component

---
+++ src/component/NavbarItem.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+export default function NavbarItem({ title, param }) {
+  const searchParam = new useSearchParams();
+  const genre = searchParam.get(`genre`);
+  return (
+    <div>
+      <Link
+        className={`hover:text-amber-600 font-semibold ${
+          genre === param
+            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+            : ""
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
+    </div>
+  );
+}
