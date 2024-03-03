df1e1fd08240487fbd811d083e74421a7e82bd49
ayuwidyainggit <widyaayu560@gmail.com>
2024-03-03 10:42:16 +0700

    fetch data from TMDB API in the home

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <div>home</div>;
+import Results from "@/component/Results";
+
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParam = {} }) {
+  const genre = searchParam.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1 `
+  );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error("Failed to fetch data ");
+  }
+  const results = data.results;
+  console.log(results);
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
