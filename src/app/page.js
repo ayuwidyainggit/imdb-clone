f558c6a6f9bc58de85e719cf73dde18b7711b8b8
ayuwidyainggit <widyaayu560@gmail.com>
2024-03-03 15:54:27 +0700

    add loading effect using nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,22 @@
 import Results from "@/component/Results";
+import { resolve } from "styled-jsx/css";
 
 const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParam = {} }) {
   const genre = searchParam.genre || "fetchTrending";
-  const res = await fetch(
-    `https://api.themoviedb.org/3${
-      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1 `
-  );
+  const res = await new Promise((resolve) => {
+    setTimeout(async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3${
+          genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+        }?api_key=${API_KEY}&language=en-US&page=1 `,
+        { next: { revalidate: 10000 } }
+      );
+      resolve(response);
+    }, 2000);
+  });
+
   const data = await res.json();
   if (!res.ok) {
     throw new Error("Failed to fetch data ");
