bb64930e994bc4cd3b3b824c770b2376d9807a7f
ayuwidyainggit <widyaayu560@gmail.com>
2024-03-06 22:50:43 +0700

    loading page

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function loading() {
   return (
     <div className="flex justify-center">
-      <Image className="h-52" src="spinner.svg" alt="loading..."></Image>
+      <img className="h-52" src="spinner.svg" alt="loading..." />
     </div>
   );
 }
